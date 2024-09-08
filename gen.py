d3306184e3c381365e0cbfc650a9f500336225df
daapiandev <briangoodwins@hotmail.com>
2024-09-08 20:19:40 +0200

    Update gen.py

--- gen.py
+++ gen.py
@@ -59,7 +59,7 @@ def generate_and_verify_account():
         print("[!]Failed to generate email and password.")
         return
 
-    time.sleep(5)  # Wait for a few seconds to ensure the email is received
+    time.sleep(3)
     inbox_url = f"https://www.1secmail.com/api/v1/?action=getMessages&login={email.split('@')[0]}&domain=1secmail.com"
     inbox_response = requests.get(inbox_url)
     messages = inbox_response.json()
